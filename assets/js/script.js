aa20b20c87f0c0a91420d8b9ecdd3d56b8f0f5fd
Jose L Riquelme <jlrseaz@gmail.com>
2024-03-20 22:33:10 -0300

    project creation using bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$('#carouselExample').carousel({
+    interval: false, // Desactivar la transición automática
+    wrap: false, // Evitar el rebobinado del carrusel
+    perPage: 4 // Mostrar 4 imágenes a la vez
+  });
+  
