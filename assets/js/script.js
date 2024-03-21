6a72c636a3b1f425897cd79b40895feb145212c6
Jose L Riquelme <jlrseaz@gmail.com>
2024-03-21 20:39:10 -0300

    adding jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-$('#carouselExample').carousel({
-    interval: false, // Desactivar la transición automática
-    wrap: false, // Evitar el rebobinado del carrusel
-    perPage: 4 // Mostrar 4 imágenes a la vez
-  });
+
+$(document).ready(function(){
+
+$("h6").dblclick(function(){
+$(this).css({
+"color": "red",
+});
+});
+
+$(".card-title").on("click",function(){
+
+  if($(".card-text").is(":visible")){
+    $(".card-text").hide();
+  }else{
+    $(".card-text").show();
+  }
+})
+$( "#enviarcorreo" ).on( "click", function() {
+  alert( "El correo fue enviado correctamente..." );
+} );
+});
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
